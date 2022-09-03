8e5935c6a51a12952c17121a82c701fb21bfc6dd
z64me <45853441+z64me@users.noreply.github.com>
2022-09-02 22:36:55 -0400

    import base64 library

--- build-win32.sh
+++ build-win32.sh
@@ -1,2 +1,2 @@
 mkdir -p bin/
-i686-w64-mingw32.static-gcc -o bin/result -Wall -Wextra -std=c99 -pedantic -Ofast -s src/*.c `i686-w64-mingw32.static-pkg-config --libs --cflags sdl2 jasper libjpeg` -lm
+i686-w64-mingw32.static-gcc -o bin/result -Wall -Wextra -std=c99 -pedantic -Ofast -s src/*.c src/base64/*.c -Isrc/base64 `i686-w64-mingw32.static-pkg-config --libs --cflags sdl2 jasper libjpeg` -lm
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 mkdir -p bin/
-gcc -o bin/result -Wall -Wextra -std=c99 -pedantic -Ofast -s src/*.c -ljasper -lSDL2 -lm
-#gcc -o bin/result -Wall -Wextra -std=c99 -pedantic -Og -g src/*.c -ljasper -lSDL2 -lm
+gcc -o bin/result -Wall -Wextra -std=c99 -pedantic -Ofast -s src/*.c src/base64/*.c -Isrc/base64 -ljasper -lSDL2 -lm
+#gcc -o bin/result -Wall -Wextra -std=c99 -pedantic -Og -g src/*.c src/base64/*.c -Isrc/base64 -ljasper -lSDL2 -lm
